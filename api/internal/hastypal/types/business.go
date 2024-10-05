d401b073afd59f5fe89e3d509bd6bbfc6076d97d
laubernal <lbernalrodrguez@gmail.com>
2024-10-05 10:44:12 +0200

    fix business definition

--- api/internal/hastypal/types/business.go
+++ api/internal/hastypal/types/business.go
@@ -4,8 +4,8 @@ type Business struct {
 	Id                 string `json:"id"`
 	Name               string `json:"name"`
 	CommunicationPhone string `json:"communicationPhone"`
-	Email              string `json:email`
-	Password           string `json:password`
+	Email              string `json:"email"`
+	Password           string `json:"password"`
 	CreatedAt          string `json:"createdAt"`
 	UpdatedAt          string `json:"updatedAt"`
 }
