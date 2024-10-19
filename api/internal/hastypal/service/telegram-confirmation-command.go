61adef8ef78ac906899d6aa37afcd64214337b78
Adriein <adria.claret@gmail.com>
2024-10-19 19:00:19 +0200

    feat(HP-12): confirm booking

---
+++ api/internal/hastypal/service/telegram-confirmation-command.go
@@ -0,0 +1,131 @@
+package service
+
+import (
+	"fmt"
+	"github.com/adriein/hastypal/internal/hastypal/constants"
+	"github.com/adriein/hastypal/internal/hastypal/helper"
+	"github.com/adriein/hastypal/internal/hastypal/types"
+	"net/url"
+	"strings"
+	"time"
+)
+
+type TelegramConfirmationCommandService struct {
+	bot *TelegramBot
+}
+
+func NewTelegramConfirmationCommandService(
+	bot *TelegramBot,
+) *TelegramConfirmationCommandService {
+	return &TelegramConfirmationCommandService{
+		bot: bot,
+	}
+}
+
+func (s *TelegramConfirmationCommandService) Execute(business types.Business, update types.TelegramUpdate) error {
+	answerCbErr := s.bot.AnswerCallbackQuery(types.AnswerCallbackQuery{CallbackQueryId: update.CallbackQuery.Id})
+
+	if answerCbErr != nil {
+		return answerCbErr
+	}
+
+	var markdownText strings.Builder
+
+	location, loadLocationErr := time.LoadLocation("Europe/Madrid")
+
+	if loadLocationErr != nil {
+		return types.ApiError{
+			Msg:      loadLocationErr.Error(),
+			Function: "Execute -> time.LoadLocation()",
+			File:     "telegram-dates-command.go",
+			Values:   []string{"Europe/Madrid"},
+		}
+	}
+
+	time.Local = location
+
+	parsedUrl, parseUrlErr := url.Parse(update.CallbackQuery.Data)
+
+	if parseUrlErr != nil {
+		return types.ApiError{
+			Msg:      parseUrlErr.Error(),
+			Function: "Execute -> url.Parse()",
+			File:     "telegram-hours-command.go",
+			Values:   []string{update.CallbackQuery.Data},
+		}
+	}
+
+	queryParams := parsedUrl.Query()
+
+	stringSelectedDate := fmt.Sprintf("%s %s", queryParams.Get("date"), "07:00:00")
+
+	selectedDate, timeParseErr := time.Parse(time.DateTime, stringSelectedDate)
+
+	if timeParseErr != nil {
+		return types.ApiError{
+			Msg:      timeParseErr.Error(),
+			Function: "Execute -> time.Parse()",
+			File:     "telegram-hours-command.go",
+			Values:   []string{stringSelectedDate},
+		}
+	}
+
+	dateParts := strings.Split(selectedDate.Format(time.RFC822), " ")
+
+	day := dateParts[0]
+	month := dateParts[1]
+
+	welcome := "*![🙂](tg://emoji?id=5368324170671202286) Último paso te lo prometo! Confirma que todo esta correcto:\n\n"
+
+	service := fmt.Sprintf(
+		"![🟢](tg://emoji?id=5368324170671202286) %s\n\n",
+		"Corte de pelo y barba express 18€",
+	)
+
+	date := fmt.Sprintf("![📅](tg://emoji?id=5368324170671202286) %s %s", day, month)
+
+	hour := fmt.Sprintf(
+		"![⌚️](tg://emoji?id=5368324170671202286) %s\n\n",
+		queryParams.Get("hour"),
+	)
+
+	processInstructions := "*Pulsa confirmar si todo es correcto o cancelar de lo contrario*\n\n"
+
+	markdownText.WriteString(welcome)
+	markdownText.WriteString(service)
+	markdownText.WriteString(date)
+	markdownText.WriteString(hour)
+	markdownText.WriteString(processInstructions)
+
+	buttons := make([]types.KeyboardButton, 2)
+
+	availableButtons := make([]string, 2)
+
+	for i := 0; i <= len(availableButtons); i++ {
+		buttons[i] = types.KeyboardButton{
+			Text: availableButtons[i],
+			CallbackData: fmt.Sprintf(
+				"/book?service=%s&date=%s&hour=%s",
+				"1",
+				selectedDate.Format(time.DateOnly),
+				queryParams.Get("hour"),
+			),
+		}
+	}
+
+	inlineKeyboard := helper.Chunk[types.KeyboardButton](buttons, 1)
+
+	message := types.SendTelegramMessage{
+		ChatId:         update.CallbackQuery.From.Id,
+		Text:           markdownText.String(),
+		ParseMode:      constants.TelegramMarkdown,
+		ProtectContent: true,
+		ReplyMarkup:    types.ReplyMarkup{InlineKeyboard: inlineKeyboard},
+	}
+
+	if botSendMsgErr := s.bot.SendMsg(message); botSendMsgErr != nil {
+		return botSendMsgErr
+	}
+
+	return nil
+}
