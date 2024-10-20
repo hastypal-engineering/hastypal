ceed1f6c8222f5bf3f3f98c8bfc17b6988b027ae
Adriein <adria.claret@gmail.com>
2024-10-20 12:10:11 +0200

    feat(HP-21): create booking session infra

---
+++ api/database/migrations/000001_create_booking_session_table.up.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS booking_session (
+    id VARCHAR(36) PRIMARY KEY, -- Unique identifier (UUID)
+    business_id VARCHAR(36) NOT NULL,
+    chat_id VARCHAR(36) NOT NULL,
+    service_id VARCHAR(36) NOT NULL,
+    date VARCHAR(60) NOT NULL,
+    hour VARCHAR(5) NOT NULL,
+    created_at VARCHAR(60) NOT NULL,
+    ttl INTEGER NOT NULL
+);
