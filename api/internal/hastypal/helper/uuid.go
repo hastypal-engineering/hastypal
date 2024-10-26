c02da08aece4fa1a8acd4389ee2d4f78f43f95be
Adriein <adria.claret@gmail.com>
2024-10-26 17:42:48 +0200

    Hotfix create short uuid helper

---
+++ api/internal/hastypal/helper/uuid.go
@@ -0,0 +1,25 @@
+package helper
+
+import (
+	"encoding/base64"
+	"github.com/google/uuid"
+	"strings"
+)
+
+type UuidHelper struct{}
+
+func NewUuidHelper() *UuidHelper {
+	return &UuidHelper{}
+}
+
+func (helper *UuidHelper) Generate() uuid.UUID {
+	return uuid.New()
+}
+
+func (helper *UuidHelper) GenerateShort() string {
+	googleUuid := uuid.New()
+
+	b64 := base64.RawURLEncoding.EncodeToString(googleUuid[:8])
+
+	return strings.TrimRight(b64, "=")
+}
