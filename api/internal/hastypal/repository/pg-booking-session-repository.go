f5863a7bf2e42800e5fd7bb50a923f8780619038
Adriein <adria.claret@gmail.com>
2024-10-27 10:59:37 +0100

    feat(HP-21): added update method in booking session repo

--- api/internal/hastypal/repository/pg-booking-session-repository.go
+++ api/internal/hastypal/repository/pg-booking-session-repository.go
@@ -195,8 +195,8 @@ func (r *PgBookingSessionRepository) Update(entity types.BookingSession) error {
 	var query strings.Builder
 
 	query.WriteString(`UPDATE booking_session `)
-	query.WriteString(`SET COLUMN id = ?, business_id = ?, chat_id = ?, service_id = ?, date = ?, hour = ?, `)
-	query.WriteString(`created_at = ?, ttl = ? WHERE id = ?`)
+	query.WriteString(`SET COLUMN id = $1, business_id = $2, chat_id = $3, service_id = $4, date = $5, hour = $6, `)
+	query.WriteString(`created_at = $7, ttl = $8 WHERE id = $1;`)
 
 	_, err := r.connection.Exec(
 		query.String(),
