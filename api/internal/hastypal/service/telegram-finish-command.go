c071f7b41530c797ec7ed103797f4c5184630de9
Adriein <adria.claret@gmail.com>
2024-10-27 19:08:51 +0100

    feat(HP-24): created finish command

--- api/internal/hastypal/service/telegram-finish-command.go
+++ api/internal/hastypal/service/telegram-finish-command.go
@@ -1,1 +1,91 @@
 package service
+
+import (
+	"github.com/adriein/hastypal/internal/hastypal/constants"
+	"github.com/adriein/hastypal/internal/hastypal/types"
+	"net/url"
+	"strings"
+)
+
+type TelegramFinishCommandService struct {
+	bot        *TelegramBot
+	repository types.Repository[types.BookingSession]
+}
+
+func NewTelegramFinishCommandService(
+	bot *TelegramBot,
+	repository types.Repository[types.BookingSession],
+) *TelegramFinishCommandService {
+	return &TelegramFinishCommandService{
+		bot:        bot,
+		repository: repository,
+	}
+}
+
+func (s *TelegramFinishCommandService) Execute(business types.Business, update types.TelegramUpdate) error {
+	if ackErr := s.ackToTelegramClient(update.CallbackQuery.Id); ackErr != nil {
+		return ackErr
+	}
+
+	var markdownText strings.Builder
+
+	parsedUrl, parseUrlErr := url.Parse(update.CallbackQuery.Data)
+
+	if parseUrlErr != nil {
+		return types.ApiError{
+			Msg:      parseUrlErr.Error(),
+			Function: "Execute -> url.Parse()",
+			File:     "telegram-finish-command.go",
+			Values:   []string{update.CallbackQuery.Data},
+		}
+	}
+
+	queryParams := parsedUrl.Query()
+
+	sessionId := queryParams.Get("session")
+
+	session, getSessionErr := s.getCurrentSession(sessionId)
+
+	if getSessionErr != nil {
+		return getSessionErr
+	}
+
+	if invalidSession := session.EnsureIsValid(); invalidSession != nil {
+		return invalidSession
+	}
+
+	message := types.SendTelegramMessage{
+		ChatId:         update.CallbackQuery.From.Id,
+		Text:           markdownText.String(),
+		ParseMode:      constants.TelegramMarkdown,
+		ProtectContent: true,
+	}
+
+	if botSendMsgErr := s.bot.SendMsg(message); botSendMsgErr != nil {
+		return botSendMsgErr
+	}
+
+	return nil
+}
+
+func (s *TelegramFinishCommandService) getCurrentSession(sessionId string) (types.BookingSession, error) {
+	filter := types.Filter{
+		Name:    "id",
+		Operand: constants.Equal,
+		Value:   sessionId,
+	}
+
+	criteria := types.Criteria{Filters: []types.Filter{filter}}
+
+	session, findOneErr := s.repository.FindOne(criteria)
+
+	if findOneErr != nil {
+		return types.BookingSession{}, findOneErr
+	}
+
+	return session, nil
+}
+
+func (s *TelegramFinishCommandService) ackToTelegramClient(callbackQueryId string) error {
+	return s.bot.AnswerCallbackQuery(types.AnswerCallbackQuery{CallbackQueryId: callbackQueryId})
+}
