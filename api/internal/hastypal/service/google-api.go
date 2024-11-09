7c3299e3d2090e59940d3a315843b30ab1f7c4d3
Adriein <adria.claret@gmail.com>
2024-11-09 18:50:05 +0100

    feat(HP-28): create the calendar client

--- api/internal/hastypal/service/google-api.go
+++ api/internal/hastypal/service/google-api.go
@@ -2,13 +2,12 @@ package service
 
 import (
 	"context"
-	"fmt"
 	"github.com/adriein/hastypal/internal/hastypal/constants"
+	"github.com/adriein/hastypal/internal/hastypal/types"
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
 	"google.golang.org/api/calendar/v3"
 	"google.golang.org/api/option"
-	"log"
 	"os"
 )
 
@@ -36,42 +35,26 @@ func (g *GoogleApi) GetAuthCodeUrl() string {
 	return config.AuthCodeURL(verifier, oauth2.AccessTypeOffline, oauth2.S256ChallengeOption(verifier))
 }
 
-func (g *GoogleApi) Client() *calendar.Service {
+func (g *GoogleApi) Client(businessToken types.GoogleToken) (*calendar.Service, error) {
 	ctx := context.Background()
 
-	config := &oauth2.Config{
-		ClientID:     os.Getenv(constants.GoogleClientId),
-		ClientSecret: os.Getenv(constants.GoogleClientSecret),
-		RedirectURL:  "http://localhost:4000/",
-		Endpoint:     google.Endpoint,
-		Scopes:       []string{calendar.CalendarEventsScope},
-	}
+	config := g.GetOauth2Config()
 
-	verifier := oauth2.GenerateVerifier()
-
-	// Redirect user to consent page to ask for permission
-	// for the scopes specified above.
-	url := config.AuthCodeURL("state", oauth2.AccessTypeOffline, oauth2.S256ChallengeOption(verifier))
-	fmt.Printf("Visit the URL for the auth dialog: %v", url)
-
-	// Use the authorization code that is pushed to the redirect
-	// URL. Exchange will do the handshake to retrieve the
-	// initial access token. The HTTP Client returned by
-	// conf.Client will refresh the token as necessary.
-	var code string
-	if _, err := fmt.Scan(&code); err != nil {
-		log.Fatal(err)
-	}
-	token, err := config.Exchange(ctx, code, oauth2.VerifierOption(verifier))
-	if err != nil {
-		log.Fatal(err)
+	token := &oauth2.Token{
+		AccessToken:  businessToken.AccessToken,
+		TokenType:    businessToken.TokenType,
+		RefreshToken: businessToken.RefreshToken,
 	}
 
 	client, newServiceErr := calendar.NewService(ctx, option.WithTokenSource(config.TokenSource(ctx, token)))
 
 	if newServiceErr != nil {
-		log.Fatal(newServiceErr)
+		return nil, types.ApiError{
+			Msg:      newServiceErr.Error(),
+			Function: "Client -> calendar.NewService()",
+			File:     "service/google-api.go",
+		}
 	}
 
-	return client
+	return client, nil
 }
