52b4f1418437030bed0e7bf27dbd18800e65b12e
laubernal <lbernalrodrguez@gmail.com>
2025-01-05 10:23:12 +0100

    feat(HP-31): wip - implement service catalog save method

--- api/internal/hastypal/shared/repository/pg-service-catalog-repository.go
+++ api/internal/hastypal/shared/repository/pg-service-catalog-repository.go
@@ -2,6 +2,7 @@ package repository
 
 import (
 	"database/sql"
+	"strings"
 
 	"github.com/adriein/hastypal/internal/hastypal/shared/exception"
 	"github.com/adriein/hastypal/internal/hastypal/shared/helper"
@@ -35,9 +36,30 @@ func (r *PgServiceCatalogRepository) FindOne(criteria types.Criteria) error {
 }
 
 func (r *PgServiceCatalogRepository) Save(entity types.ServiceCatalog) error {
-	return exception.
-		New("Method not implemented").
-		Trace("Save", "pg-service-catalog-repository.go")
+	var query strings.Builder
+
+	query.WriteString(`INSERT INTO service_catalog `)
+	query.WriteString(`(id, name, price, currency, duration, business_id) `)
+	query.WriteString(`VALUES ($1, $2, $3, $4, $5, $6);`)
+
+	_, err := r.connection.Exec(
+		query.String(),
+		entity.Id,
+		entity.Name,
+		entity.Price,
+		entity.Currency,
+		entity.Duration,
+		entity.BusinessId,
+	)
+
+	if err != nil {
+		return exception.
+			New(err.Error()).
+			Trace("r.connection.Exec", "pg-service-catalog-repository.go").
+			WithValues([]string{query.String(), entity.Id, entity.Name, entity.BusinessId})
+	}
+
+	return nil
 }
 
 func (r *PgServiceCatalogRepository) Update(entity types.ServiceCatalog) error {
