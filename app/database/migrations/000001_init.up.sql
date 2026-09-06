0f07b74e26d23da2976275ff5b9734b92082fdee
Adria Claret <adria.claret@gmail.com>
2026-09-06 10:29:15 +0200

    feat(HP-26): comment tables i do not want to create yet

--- app/database/migrations/000001_init.up.sql
+++ app/database/migrations/000001_init.up.sql
@@ -29,76 +29,76 @@ CREATE TABLE IF NOT EXISTS ha_service_catalog (
     CONSTRAINT fk_service_catalog_business FOREIGN KEY(hasc_business_id) REFERENCES ha_business(hab_id)
 );
 
-CREATE TABLE IF NOT EXISTS ha_employees (
-    hae_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    hae_name VARCHAR(255) NOT NULL,
-    hae_business_id BIGINT NOT NULL,
-    hae_date_add TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL,
-    hae_date_upd TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL,
-    CONSTRAINT fk_employees_business FOREIGN KEY(hae_business_id) REFERENCES ha_business(hab_id)
-)
+-- CREATE TABLE IF NOT EXISTS ha_employees (
+--     hae_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+--     hae_name VARCHAR(255) NOT NULL,
+--     hae_business_id BIGINT NOT NULL,
+--     hae_date_add TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL,
+--     hae_date_upd TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL,
+--     CONSTRAINT fk_employees_business FOREIGN KEY(hae_business_id) REFERENCES ha_business(hab_id)
+-- )
 
-CREATE TABLE IF NOT EXISTS ha_open_hours (
-    haoh_business_id BIGINT,
-    haoh_date_add TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL,
-    haoh_date_upd TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL,
-    CONSTRAINT fk_service_catalog_business FOREIGN KEY(haoh_business_id) REFERENCES ha_business(hab_id)
-)
+-- CREATE TABLE IF NOT EXISTS ha_open_hours (
+--     haoh_business_id BIGINT,
+--     haoh_date_add TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL,
+--     haoh_date_upd TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL,
+--     CONSTRAINT fk_service_catalog_business FOREIGN KEY(haoh_business_id) REFERENCES ha_business(hab_id)
+-- )
 
-CREATE TABLE IF NOT EXISTS ha_business_holidays (
-    habh_business_id BIGINT,
-    habh_date_add TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL,
-    habh_date_upd TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL,
-    CONSTRAINT fk_service_catalog_business FOREIGN KEY(habh_business_id) REFERENCES ha_business(hab_id)
-)
+-- CREATE TABLE IF NOT EXISTS ha_business_holidays (
+--     habh_business_id BIGINT,
+--     habh_date_add TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL,
+--     habh_date_upd TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL,
+--     CONSTRAINT fk_service_catalog_business FOREIGN KEY(habh_business_id) REFERENCES ha_business(hab_id)
+-- )
 
-CREATE TABLE IF NOT EXISTS booking_session (
-    habs_id VARCHAR(36) PRIMARY KEY,
-    habs_business_id VARCHAR(36) NOT NULL,
-    habs_service_id VARCHAR(36) NOT NULL,
-    habs_date VARCHAR(60) NOT NULL,
-    habs_hour VARCHAR(5) NOT NULL,
-    habs_ttl INTEGER NOT NULL,
-    habs_date_add VARCHAR(60) NOT NULL,
-    habs_date_upd VARCHAR(60) NOT NULL,
-    CONSTRAINT fk_booking_session_business FOREIGN KEY(habs_business_id) REFERENCES ha_business(hab_id)
-);
-
-CREATE TABLE IF NOT EXISTS booking (
-    id VARCHAR(36) PRIMARY KEY,
-    session_id VARCHAR(8) NOT NULL,
-    business_id VARCHAR(36) NOT NULL,
-    service_id VARCHAR(36) NOT NULL,
-    booking_date VARCHAR(60) NOT NULL,
-    created_at VARCHAR(60) NOT NULL,
-    FOREIGN KEY (session_id) REFERENCES booking_session(id),
-    FOREIGN KEY (business_id) REFERENCES business(id)
-);
-
-CREATE TABLE IF NOT EXISTS telegram_notification (
-    id VARCHAR(36) PRIMARY KEY,
-    session_id VARCHAR(8) NOT NULL,
-    booking_id VARCHAR(36) NOT NULL,
-    business_id VARCHAR(36) NOT NULL,
-    scheduled_at VARCHAR(60) NOT NULL,
-    chat_id INT NOT NULL,
-    business_name VARCHAR(255) NOT NULL,
-    service_name VARCHAR(255) NOT NULL,
-    booking_date VARCHAR(60) NOT NULL,
-    sent BOOLEAN NOT NULL,
-    sent_at VARCHAR(60) NULL,
-    created_at VARCHAR(60) NOT NULL,
-    FOREIGN KEY (session_id) REFERENCES booking_session(id),
-    FOREIGN KEY (booking_id) REFERENCES booking(id),
-    FOREIGN KEY (business_id) REFERENCES business(id)
-);
-
-CREATE TABLE IF NOT EXISTS google_token (
-    business_id VARCHAR(36) PRIMARY KEY, -- Unique identifier (UUID)
-    access_token VARCHAR(255) NOT NULL,
-    token_type VARCHAR(255) NOT NULL,
-    refresh_token VARCHAR(255) NOT NULL,
-    created_at VARCHAR(60) NOT NULL,
-    updated_at VARCHAR(60) NOT NULL,
-    FOREIGN KEY (business_id) REFERENCES business(id)
-);
+-- CREATE TABLE IF NOT EXISTS booking_session (
+--     habs_id VARCHAR(36) PRIMARY KEY,
+--     habs_business_id VARCHAR(36) NOT NULL,
+--     habs_service_id VARCHAR(36) NOT NULL,
+--     habs_date VARCHAR(60) NOT NULL,
+--     habs_hour VARCHAR(5) NOT NULL,
+--     habs_ttl INTEGER NOT NULL,
+--     habs_date_add VARCHAR(60) NOT NULL,
+--     habs_date_upd VARCHAR(60) NOT NULL,
+--     CONSTRAINT fk_booking_session_business FOREIGN KEY(habs_business_id) REFERENCES ha_business(hab_id)
+-- );
+--
+-- CREATE TABLE IF NOT EXISTS booking (
+--     id VARCHAR(36) PRIMARY KEY,
+--     session_id VARCHAR(8) NOT NULL,
+--     business_id VARCHAR(36) NOT NULL,
+--     service_id VARCHAR(36) NOT NULL,
+--     booking_date VARCHAR(60) NOT NULL,
+--     created_at VARCHAR(60) NOT NULL,
+--     FOREIGN KEY (session_id) REFERENCES booking_session(id),
+--     FOREIGN KEY (business_id) REFERENCES business(id)
+-- );
+--
+-- CREATE TABLE IF NOT EXISTS telegram_notification (
+--     id VARCHAR(36) PRIMARY KEY,
+--     session_id VARCHAR(8) NOT NULL,
+--     booking_id VARCHAR(36) NOT NULL,
+--     business_id VARCHAR(36) NOT NULL,
+--     scheduled_at VARCHAR(60) NOT NULL,
+--     chat_id INT NOT NULL,
+--     business_name VARCHAR(255) NOT NULL,
+--     service_name VARCHAR(255) NOT NULL,
+--     booking_date VARCHAR(60) NOT NULL,
+--     sent BOOLEAN NOT NULL,
+--     sent_at VARCHAR(60) NULL,
+--     created_at VARCHAR(60) NOT NULL,
+--     FOREIGN KEY (session_id) REFERENCES booking_session(id),
+--     FOREIGN KEY (booking_id) REFERENCES booking(id),
+--     FOREIGN KEY (business_id) REFERENCES business(id)
+-- );
+--
+-- CREATE TABLE IF NOT EXISTS google_token (
+--     business_id VARCHAR(36) PRIMARY KEY, -- Unique identifier (UUID)
+--     access_token VARCHAR(255) NOT NULL,
+--     token_type VARCHAR(255) NOT NULL,
+--     refresh_token VARCHAR(255) NOT NULL,
+--     created_at VARCHAR(60) NOT NULL,
+--     updated_at VARCHAR(60) NOT NULL,
+--     FOREIGN KEY (business_id) REFERENCES business(id)
+-- );
