e576e2deb3bf43bd3e931669c06bc59be1ba5fca
Adria Claret <adria.claret@gmail.com>
2026-09-19 10:14:12 +0200

    feat(HP-26): remove useless css

--- app/ui/html/step_1_templ.go
+++ app/ui/html/step_1_templ.go
@@ -32,7 +32,7 @@ func Step1(dto *web.BookingDTO) templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<html><head><meta charset=\"UTF-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, viewport-fit=cover\"><title>Hastypal</title><link rel=\"icon\" type=\"image/x-icon\" href=\"/ui/static/images/golden_helmet.ico\"><link rel=\"stylesheet\" href=\"/ui/static/css/main.css\"></head><body class=\"main-content\"><main><header class=\"header\"><div class=\"header__content\"><div class=\"header__brand\"><div class=\"header__logo\" aria-hidden=\"true\">H</div><div class=\"header__brand-text\"><p class=\"header__name\">")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<html><head><meta charset=\"UTF-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, viewport-fit=cover\"><title>Hastypal</title><link rel=\"icon\" type=\"image/x-icon\" href=\"/ui/static/images/golden_helmet.ico\"><link rel=\"stylesheet\" href=\"/ui/static/css/main.css\"></head><body><main><header class=\"header\"><div class=\"header__content\"><div class=\"header__brand\"><div class=\"header__logo\" aria-hidden=\"true\">H</div><div class=\"header__brand-text\"><p class=\"header__name\">")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
